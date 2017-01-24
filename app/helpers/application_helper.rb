a131419b23e8ebd20b996837e2b2f9e1f78d8217
Cliff H <cliffhazelton@gmail.com>
2017-01-24 17:23:44 -0500

    Render links all fancy in feature/note/reminder descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def render_links_in(string)
+    string.split(" ").map { |piece| piece.match(/^http/) ? link_to(piece, piece, target: "_external") : piece }.join(" ")
+  end
+
 end
