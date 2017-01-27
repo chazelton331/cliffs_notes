2190e053ca86672d5b7e4abea0de4ddb2c750aad
Cliff H <cliffhazelton@gmail.com>
2017-01-27 10:19:14 -0500

    Added annotate headers to every other model record/fixture/test.

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: features
+#
+#  id          :integer          not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  priority    :integer          default("0"), not null
+#  description :text
+#  squad       :string
+#
+
 class Feature < ApplicationRecord
 end
--- test/models/feature_test.rb
+++ test/models/feature_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: features
+#
+#  id          :integer          not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  priority    :integer          default("0"), not null
+#  description :text
+#  squad       :string
+#
+
 require 'test_helper'
 
 class FeatureTest < ActiveSupport::TestCase
