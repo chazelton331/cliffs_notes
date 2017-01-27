e88d77f312fae6637dcae64b1ed0f80f348942b2
Cliff H <cliffhazelton@gmail.com>
2017-01-27 10:05:38 -0500

    Added validations and tests for the Note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id          :integer          not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  description :text
+#
+
 class Note < ApplicationRecord
+
+  DESCRIPTION_LIMIT = 5000
+
+  validates :description,
+            presence: true,
+            length: { maximum: DESCRIPTION_LIMIT }
+
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,34 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id          :integer          not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  description :text
+#
+
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "description too long" do
+    bad_note = Note.new(description: 'a' * (Note::DESCRIPTION_LIMIT + 1))
+
+    assert(bad_note.invalid?)
+
+    assert_equal([ "Description is too long (maximum is #{Note::DESCRIPTION_LIMIT} characters)" ], bad_note.errors.full_messages)
+  end
+
+  test "description required" do
+    bad_note1 = Note.new(description: nil)
+    bad_note2 = Note.new(description: "" )
+
+    assert(bad_note1.invalid?)
+    assert(bad_note2.invalid?)
+
+    assert_equal([ "Description can't be blank" ], bad_note1.errors.full_messages)
+    assert_equal([ "Description can't be blank" ], bad_note2.errors.full_messages)
+  end
+
 end
